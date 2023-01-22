219627d0b16ef9f799d0a0049a294acf050e0eac
Chiranthan <Chiranthangowda4224@gmail.com>
2023-01-22 18:47:45 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+
+
+// import './App.css';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Layout from "./pages/Layout";
+// import Home from "./pages/Home";
+// import Blogs from "./pages/Blogs";
+// // import Contact from "./pages/Contact";
+// import NoPage from "./pages/NoPage";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+// <Routes>
+// <Route path="/" element={<Layout />}>
+// <Route index element={<Home />} />
+// <Route path="blogs" element={<Blogs />} />
+// {/* <Route path="contact" element={<Contact />} /> */}
+// <Route path="*" element={<NoPage />} />
+// </Route>
+// </Routes>
+// </BrowserRouter>
+    
+//   );
+// }
+
+// export default App;
+
+
+// import {BrowserRouter as Router,Route,Routes,Link}from "react-router-dom";
+// import Home from "./Component/Home";
+// import Contact from "./Component/Contact";
+// import About from "./Component/About";
+
+// import { Component } from "react";
+// class App extends Component{
+//   render(){
+//     return(
+//       <Router>
+//         <div className="App">
+//           <ul className="App=header">
+//             <li>
+//               <Link to="Home/">Home</Link>
+//             </li>
+//             <li>
+//               <Link to="Contact/">Contact</Link>
+//             </li>
+//             <li>
+//               <Link to="About">About</Link>
+//             </li>
+//           </ul>
+//           <Routes>
+//             <Route exact path="/Home" element={<Home/>}></Route>
+//             <Route exact path="/Contact" element={<Contact/>}></Route>
+//             <Route exact path="/About" element={<About/>}></Route>
+//           </Routes>
+//         </div>
+//       </Router>
+//     );
+//   }
+// }
+// export default App;
